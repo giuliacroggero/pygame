74082847a6bcf3ce06a040415070d683eb563d75
Giulia <giuliar1@al.insper.edu.br>
2022-11-21 18:40:03 -0300

    adicionando variaveis

--- Zjogo.py
+++ Zjogo.py
@@ -10,20 +10,26 @@ pygame.display.set_caption('BellyBaloo')
 
 game = True
 
-
+#colocando imagens
 image = pygame.image.load('assets/img/menu.png').convert()
 image = pygame.transform.scale(image, (400, 600))
 imageplay = pygame.image.load('assets/img/play.png').convert_alpha()
 imageplay = pygame.transform.scale(imageplay, (200, 100))
+imagefundojogo=pygame.image.load('assets/img/fundojogo.png').convert()
+imagefundojogo = pygame.transform.scale(imagefundojogo, (400, 600))
 
 while game:
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT:
-            game = False
     window.fill((255, 153, 255))  # Preenche com a cor
     window.blit(image, (0, 0))
     window.blit(imageplay,(100,200))
-
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            window.blit(imagefundojogo, (0, 0))
+    #colocando imagem do play
+    
+        
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
