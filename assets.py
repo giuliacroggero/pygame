5ad45991443dde52f608ede05431c648dcca232e
Giulia <giuliar1@al.insper.edu.br>
2022-11-21 18:55:20 -0300

    salvei

--- assets.py
+++ assets.py
@@ -1,11 +1,15 @@
 import pygame
 import os
-from config import MENU_WIDTH, MENU_HEIGHT, IMG_DIR, SND_DIR, FNT_DIR
+from config import MENU_WIDTH, MENU_HEIGHT, BALOO_WIDTH, BALOO_HEIGHT, IMG_DIR, SND_DIR, FNT_DIR
 
 MENU_IMG = 'menu_img'
+BALOO_IMG = 'baloo_img'
 
 def load_assets():
     assets = {}
     assets[MENU_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'menu.png')).convert_alpha()
     assets[MENU_IMG] = pygame.transform.scale(assets['menu_img'], (MENU_WIDTH, MENU_HEIGHT))
+    assets[BALOO_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'baloo.png')).convert_alpha()
+    assets[BALOO_IMG] = pygame.transform.scale(assets['baloo_img'], (BALOO_WIDTH, BALOO_HEIGHT))
+
     return assets
--- config.py
+++ config.py
@@ -7,8 +7,11 @@ FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
 # Dados gerais do jogo.
 WIDTH = 400 # Largura da tela
 HEIGHT = 600 # Altura da tela
+FPS = 60
 MENU_WIDTH = 400
 MENU_HEIGHT = 600
+BALOO_WIDTH = 40
+BALOO_HEIGHT = 60
 
 
 WHITE = (255, 255, 255)
